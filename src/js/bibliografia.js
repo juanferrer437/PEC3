ef275b9f1d0dac99f8228a98b8251208de6c79af
juanferrer437 <juanferrer437@uoc.edu>
2023-01-20 19:52:39 +0100

    u

--- src/js/bibliografia.js
+++ src/js/bibliografia.js
@@ -3,7 +3,7 @@ let salida = "<table><thead><th>Imagen</th><th>Año</th><th>Título no original 
 for (let element of jsonData) {
     if (element.id % 2 == 0) {
         if (element.id==14) {
-            salida += `<tr><td><a href="./detalle.html"><img class="icono" src="https://dainty-starburst-7bf821.netlify.app/logodetalle2.1b7c0a85.webp"></a></td><td><a href="./detalle.html">${element.date}</a></td><td><a href="./detalle.html">${element.titulo}</a></td><td><a href="./detalle.html">${element.original}</a></td></tr>`;        
+            salida += `<tr><td><a href="./detalle.html"><img alt="icono" class="icono" src="https://dainty-starburst-7bf821.netlify.app/logodetalle2.1b7c0a85.webp"></a></td><td><a href="./detalle.html">${element.date}</a></td><td><a href="./detalle.html">${element.titulo}</a></td><td><a href="./detalle.html">${element.original}</a></td></tr>`;        
         }
         else {
         salida += `<tr><td></td><td>${element.date}</td><td>${element.titulo}</td><td>${element.original}</td></tr>`;
