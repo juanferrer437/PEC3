d6890c7c9e9d7ff858e7ff9230cd7f2eea147345
juanferrer437 <juanferrer437@uoc.edu>
2023-01-20 21:20:40 +0100

    33

--- src/js/bibliografia.js
+++ src/js/bibliografia.js
@@ -1,17 +1,18 @@
 const jsonData = require('./obras.json');
-let salida = ` <table>
-<thead><th>Imagen</th><th>Año</th><th>Título no original (español)</th><th>Título original</th></thead><tbody>`;
+let salida = ` <table class="colapsado">
+<thead><th>Imagen</th><th>Año</th><th>Título no original (español)</th><th>Título original</th></thead><tbody></tbody>`;
 for (let element of jsonData) {
         if (element.id==14) {
-            salida += `<tr ><td><a href="./detalle.html"><img alt="icono" class="icono" src="https://dainty-starburst-7bf821.netlify.app/logodetalle2.1b7c0a85.webp"></a></td><td><a href="./detalle.html">${element.date}</a></td><td><a href="./detalle.html">${element.titulo}</a></td><td><a href="./detalle.html">${element.original}</a></td></tr>`;        
+            salida += `<tr><td><a href="./detalle.html"><img alt="icono" class="icono" src="https://dainty-starburst-7bf821.netlify.app/logodetalle2.1b7c0a85.webp"></a></td><td><a href="./detalle.html">${element.date}</a></td><td><a href="./detalle.html">${element.titulo}</a></td><td><a href="./detalle.html">${element.original}</a></td></tr>`;        
         }
         else {
         salida += `<tr><td></td><td>${element.date}</td><td>${element.titulo}</td><td>${element.original}</td></tr>`;
     }
 }   
     
-salida+=`</tbody></table>`;
+salida+=`</table>`;
 document.querySelector("#tabla").innerHTML=salida;
+
 
 
 
